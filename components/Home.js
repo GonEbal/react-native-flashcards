8b0289cdb50248b28a72867c3f82183a06dac872
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-02-03 15:07:05 -0800

    Home components

---
+++ components/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, Text} from 'react-native'
+import {YELLOW} from "../utils/colors";
+
+class Home extends Component {
+
+    render() {
+        return (
+            <View >
+                <Text>Home</Text>
+            </View>
+        );
+    }
+}
+
+
+export default Home
