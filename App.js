25e8e66671cfcb2a773e3f4e2bdc0b2160203542
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-02-03 15:10:23 -0800

    AddCard

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import Home from "./components/Home";
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { NavigationContainer } from "@react-navigation/native"
+import DeckList from "./components/DeckList"
+import AddCard from "./components/AddCard"
 
 export default function App() {
   return (
     <View style={styles.container}>
-        <Home />
-        <StatusBar style="auto" />
+      <DeckList />
+      <AddCard />
+      <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -20,4 +22,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-});
+})
---
+++ components/DeckList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import { View, StyleSheet, Text } from "react-native"
+import { YELLOW } from "../utils/colors"
+
+class DeckList extends Component {
+	render() {
+		return (
+			<View>
+				<Text>DeckList</Text>
+			</View>
+		)
+	}
+}
+
+export default DeckList
