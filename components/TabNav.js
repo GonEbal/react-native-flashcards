dc883d230fd24181105f4b779e9e19ad2c0538a3
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-02-03 21:55:17 -0800

    TabNav Style

--- components/TabNav.js
+++ components/TabNav.js
@@ -1,49 +1,44 @@
-import React from "react";
-import {
-    FontAwesome,
-    Ionicons,
-    MaterialCommunityIcons,
-} from "@expo/vector-icons";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import DeckList from "../screens/DeckList";
-import { LightBlue, Gray } from "../utils/colors";
-import AddDeck from "../screens/AddDeck";
-import { Platform } from "react-native";
+import React from "react"
+import { Ionicons } from "@expo/vector-icons"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs"
+import DeckList from "../screens/DeckList"
+import { LightBlue, Gray } from "../utils/colors"
+import AddDeck from "../screens/AddDeck"
+import { Platform } from "react-native"
 
 const Tabs =
     Platform.OS === "ios"
         ? createBottomTabNavigator()
-        : createMaterialTopTabNavigator();
+        : createMaterialTopTabNavigator()
 
 function TabNav() {
     return (
         <Tabs.Navigator
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ focused, color, size }) => {
-                    let iconName;
+                    let iconName
 
                     if (route.name === "Deck List") {
-                        iconName = focused
-                            ? "ios-bookmarks"
-                            : "ios-bookmarks-outline";
+                        iconName = focused ? "albums" : "albums-outline"
                     } else if (route.name === "Add Deck") {
-                        iconName = focused
-                            ? "add-circle"
-                            : "add-circle-outline";
+                        iconName = focused ? "duplicate" : "duplicate-outline"
                     }
-                    return <Ionicons name={iconName} size={30} color={color} />;
+                    return <Ionicons name={iconName} size={30} color={color} />
                 },
             })}
             tabBarOptions={{
                 activeTintColor: LightBlue,
                 inactiveTintColor: Gray,
+                style: {
+                    height: Platform.OS === "ios" ? 80 : 50,
+                },
             }}
         >
             <Tabs.Screen name="Deck List" component={DeckList} />
             <Tabs.Screen name="Add Deck" component={AddDeck} />
         </Tabs.Navigator>
-    );
+    )
 }
 
-export default TabNav;
+export default TabNav
