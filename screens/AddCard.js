68a3ba1eef7107263ca02a7bed238eeaa0ef1bb2
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-02-05 14:31:00 -0800

    AddCard CSS

--- screens/AddCard.js
+++ screens/AddCard.js
@@ -1,32 +1,65 @@
-import React, { Component } from "react"
-import { View, StyleSheet, Text, TextInput } from "react-native"
-import TouchButton from "../components/TouchButton"
-import { Gray, Main } from "../utils/colors"
+import React, { Component } from "react";
+import { View, StyleSheet, Text, TextInput } from "react-native";
+import TouchButton from "../components/TouchButton";
+import { Gray, Main } from "../utils/colors";
 
 class AddCard extends Component {
 	state = {
 		question: "",
 		answer: "",
-	}
+	};
 	render() {
 		return (
-			<View>
+			<View style={styles.container}>
 				<View>
 					<View>
-						<Text>Add a question</Text>
+						<Text style={styles.title}>Add a question</Text>
 					</View>
 					<View>
-						<TextInput placeholder="Question" />
+						<TextInput
+							placeholder="Question"
+							style={styles.input}
+							autoFocus={true} 
+							
+						/>
 					</View>
 					<View>
-						<TextInput placeholder="Answer" />
+						<TextInput 
+						placeholder="Answer" 
+						style={styles.input} 
+						
+						/>
 					</View>
 					<TouchButton>Submit</TouchButton>
 				</View>
-				<View/>
+				<View />
 			</View>
-		)
+		);
 	}
 }
 
-export default AddCard
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		justifyContent: "space-around",
+	},
+	title: {
+		textAlign: "center",
+		fontSize: 32,
+		marginBottom: 20,
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: "gray",
+		backgroundColor: "#fff",
+		paddingLeft: 10,
+		paddingRight: 10,
+		borderRadius: 5,
+		fontSize: 20,
+		height: 40,
+		marginBottom: 20,
+	},
+});
+
+export default AddCard;
