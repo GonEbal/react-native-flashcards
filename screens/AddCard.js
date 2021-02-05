ff042c57728acab96395ac4e1f9c01a5a6296175
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-02-05 14:19:24 -0800

    Update AddCard.js

--- screens/AddCard.js
+++ screens/AddCard.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react"
-import { View, StyleSheet, Text } from "react-native"
-import { YELLOW } from "../utils/colors"
+import { View, StyleSheet, Text, TextInput } from "react-native"
+import TouchButton from "../components/TouchButton"
+import { Gray, Main } from "../utils/colors"
 
 class AddCard extends Component {
+	state = {
+		question: "",
+		answer: "",
+	}
 	render() {
 		return (
-			<View style={{ flex: 1 }}>
-				<Text>Add Card</Text>
+			<View>
+				<View>
+					<View>
+						<Text>Add a question</Text>
+					</View>
+					<View>
+						<TextInput placeholder="Question" />
+					</View>
+					<View>
+						<TextInput placeholder="Answer" />
+					</View>
+					<TouchButton>Submit</TouchButton>
+				</View>
+				<View/>
 			</View>
 		)
 	}
