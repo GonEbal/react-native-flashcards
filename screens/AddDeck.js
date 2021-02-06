ed4c97da0e6c4638521f2133085ee19c3ba4d47a
GonEbal <66447247+GonEbal@users.noreply.github.com>
2021-02-06 12:03:00 -0800

    Update AddDeck.js

--- screens/AddDeck.js
+++ screens/AddDeck.js
@@ -1,15 +1,45 @@
-import React, { Component } from "react"
-import { View, StyleSheet, Text } from "react-native"
-import { YELLOW } from "../utils/colors"
+import React, { Component } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import {BLACK, Main, White, Gray} from "../utils/colors";
+import TouchButton from "../components/TouchButton"
 
 class AddDeck extends Component {
 	render() {
 		return (
-			<View style={{ flex: 1 }}>
-				<Text>Add Deck</Text>
+			<View style={styles.container}>
+				<Text style={styles.title}>
+					What is the title of your new deck?
+				</Text>
+				<TextInput
+					placeholder="Deck Title"
+					placeholderTextColor={BLACK}
+					style={styles.inputField}
+				/>
+				<TouchButton>Submit</TouchButton>
 			</View>
-		)
+		);
 	}
 }
 
-export default AddDeck
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 20,
+		justifyContent: "center",
+	},
+	inputField: {
+		height: 40,
+		padding: 10,
+		marginBottom: 30,
+		borderColor: Gray,
+		borderWidth: 1,
+	},
+	title: {
+		color: Main,
+		fontSize: 36,
+		textAlign: "center",
+		marginBottom: 30,
+	},
+});
+
+export default AddDeck;
